316f494bfa373a6caf38104630a4b2dc47bfb9de
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-07-27 11:40:39 +0600

    how to find factors of a given number

---
+++ prob-15.js
@@ -0,0 +1,14 @@
+//how to find factors of a given integer
+
+
+const func=(num)=>{
+    let result=0
+    for(let i=0; i<=num;i++){
+        if(num%i==0){
+console.log(i)
+        }
+    }
+ 
+}
+
+console.log(func(13))
